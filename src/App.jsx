beb8be46cff05b9fc0cebaa8c49f35b4e8ba72c2
Raghavan258 <venkataraghavansreenivasan@gmail.com>
2025-07-23 10:02:38 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>S.Venkata Raghavan <br /></h1>
+        <h2>Id-2400080047 <br /></h2>
+        <h2>Branch-Ai&Ds</h2>
+        <h3>Subject-Front End Development Frameworks</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
